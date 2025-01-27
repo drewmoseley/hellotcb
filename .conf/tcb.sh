d8519a5b0eb45561ecaade7ec8f474b1e8fc9192
Drew Moseley <drew@moseleynet.net>
2025-01-27 16:59:49 -0500

    More debug
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- .conf/tcb.sh
+++ .conf/tcb.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+echo "TCB_SH_DEBUG: $*"
+echo "TCB_SH_DEBUG: VSCODE_CMD=${VSCODE_CMD}"
+pwd
+ls -l
+id
+
 TCB_ENV="$(pwd)/.conf/tcb-env-setup.sh"
+echo "TCB_SH_DEBUG: cmd: source ${TCB_ENV} -s ${STORAGE_FOLDER} -t ${TCB_VERSION}"
+
 source ${TCB_ENV} -s ${STORAGE_FOLDER} -t ${TCB_VERSION}
